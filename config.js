803b734a63096afdaa917921b3f9735296ad0689
hanzo2001 <bilbolord2001@gmail.com>
2018-09-15 19:15:13 +0100

    refactor(config): improve readability

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 
-const environments = {};
-
-environments.staging = {
-	httpPort: 3000,
-	httpsPort: 3001,
-	x509key: './https/key.pem',
-	x509cert: './https/cert.pem',
-	envName: 'staging'
-};
-
-environments.production = {
-	httpPort: 5000,
-	httpsPort: 5001,
-	x509key: './https/key.pem',
-	x509cert: './https/cert.pem',
-	envName: 'production'
+const environments = {
+	staging: {
+		httpPort: 3000,
+		httpsPort: 3001,
+		x509key: './https/key.pem',
+		x509cert: './https/cert.pem',
+		envName: 'staging'
+	},
+	production: {
+		httpPort: 5000,
+		httpsPort: 5001,
+		x509key: './https/key.pem',
+		x509cert: './https/cert.pem',
+		envName: 'production'
+	},
 };
 
 const currentMode = process.env.NODE_ENV;
