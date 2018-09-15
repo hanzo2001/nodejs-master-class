b786f0147914e5538614ffe466764ac80494a683
hanzo2001 <bilbolord2001@gmail.com>
2018-09-15 19:43:44 +0100

    doc(DefaultRouter): add explanation comments

--- server/DefaultRouter.js
+++ server/DefaultRouter.js
@@ -2,11 +2,17 @@ const { rawResponseWriter } = require("./responseWriters");
 
 const { errorHandlers } = require("./errorHandlers");
 
+/*
+ * The default router sets up a catch-all route that maps to a 500 error response
+ */
 class DefaultRouter {
-	resolve(path, request) {
+	// resolves a path to handler. Called by the server
+	resolve(path, userRequestData) {
+		const { method } = userRequestData;
 		const data = {method, path};
+		// return the handler that ignores user data and just echoes the method and path
 		return (_, serverResponse) => {
-			console.log(this.constructor.name, request);
+			console.log(this.constructor.name, userRequestData);
 			errorHandlers[500](data, rawResponseWriter(serverResponse));
 		}
 	}
