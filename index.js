caf80135740865cda7b27f3fbe7a2e92a5f18958
hanzo2001 <bilbolord2001@gmail.com>
2018-09-15 19:16:06 +0100

    doc(index): add explanation comments

--- index.js
+++ index.js
@@ -10,18 +10,27 @@ const { x509key, x509cert, httpPort, envName, httpsPort } = require("./config");
 
 const { routes } = require("./routes");
 
+// prepare a router to manage available routes
 const router = new Router();
+
+// setup the unified server with the router
 const server = new UnifiedServer(router);
 
+// secure options requirements
 const httpsServerOptions = {
 	key: fs.readFileSync(x509key),
 	cert: fs.readFileSync(x509cert)
 };
 
+// prepare a secure server
 const httpsServer = https.createServer(httpsServerOptions, (request, response) => server.serve(true, request, response));
+
+// prepare a normal server
 const httpServer = http.createServer((request, response) => server.serve(false, request, response));
 
+// add all the routes to the router
 routes.forEach(r => router[r.method](new Route(r.secure, r.path, r.handler, r.writer)));
 
+// start the real application
 httpsServer.listen(httpsPort, () => console.log(`Listening securely on '${envName}' port ${httpsPort}`));
-httpServer.listen(httpPort, () => console.log(`Listening on '${envName}' port ${httpPort}`));
+httpServer.listen(httpPort, () => console.log(`Listening normally on '${envName}' port ${httpPort}`));
